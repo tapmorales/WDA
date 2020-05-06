214d5d0d2264a2abacc02b7d03435304b585a69f
fellipefreiire <fellipefreiire3@gmail.com>
2020-05-06 18:51:11 -0300

    pages

--- src/js/html5.js
+++ src/js/html5.js
@@ -3,18 +3,3 @@ const $content = document.querySelector(".content");
 
 console.log($content);
 console.log($content.innerHTML);
-
-$content.addEventListener("click", function (event) {
-	let targetElement = event.target;
-	if (targetElement.innerText === "<!DOCTYPE html>") {
-		$content.innerHTML = `
-				<div class="menu">
-					<ul>
-						<li>Início</li>
-						<li>Sobre</li>
-						<li>Contato</li>
-					</ul>
-				</div>
-			`;
-	}
-});
