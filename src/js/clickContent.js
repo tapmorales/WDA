8d68a3686e07b1905848abdab48c0dcffc98e4fe
fellipefreiire <fellipefreiire3@gmail.com>
2020-05-07 03:35:22 -0300

    getting better

--- src/js/clickContent.js
+++ src/js/clickContent.js
@@ -1,42 +1,33 @@
-var $content__item = document.querySelectorAll(".content__item");
-var $content = document.querySelector(".content");
-var $contentHtml = document.querySelector(".content-html");
-var $contentCss = document.querySelector(".content-css");
-var $contentJs = document.querySelector(".content-js");
-var $contentNode = document.querySelector(".content-node");
-var $contentNpm = document.querySelector(".content-npm");
-var $contentSass = document.querySelector(".content-sass");
-var $contentGulp = document.querySelector(".content-gulp");
-var $contentRollup = document.querySelector(".content-rollup");
-var $contentWebpack = document.querySelector(".content-webpack");
+const $content = document.querySelector(".content");
+
+const $contentHtml = document.querySelector(".content-html");
+const $contentCss = document.querySelector(".content-css");
+const $contentJs = document.querySelector(".content-js");
+const $contentNode = document.querySelector(".content-node");
+const $contentNpm = document.querySelector(".content-npm");
+const $contentSass = document.querySelector(".content-sass");
+const $contentGulp = document.querySelector(".content-gulp");
+const $contentRollup = document.querySelector(".content-rollup");
+const $contentWebpack = document.querySelector(".content-webpack");
+
+const contentClick = [
+	$contentHtml,
+	$contentCss,
+	$contentJs,
+	$contentNode,
+	$contentNpm,
+	$contentSass,
+	$contentGulp,
+	$contentRollup,
+	$contentWebpack,
+];
 
 $content.addEventListener("click", function (e) {
 	$content.className += " hidden";
-	if (e.target.parentNode.id === "html5-content") {
-		$contentHtml.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "css3-content") {
-		$contentCss.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "js-content") {
-		$contentJs.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "node-content") {
-		$contentNode.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "npm-content") {
-		$contentNpm.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "sass-content") {
-		$contentSass.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "gulp-content") {
-		$contentGulp.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "rollup-content") {
-		$contentRollup.classList.remove("hidden");
-	}
-	if (e.target.parentNode.id === "webpack-content") {
-		$contentWebpack.classList.remove("hidden");
+
+	for (let i = 0; i < contentClick.length; i++) {
+		if (e.target.parentNode.id === $content.children[i].id) {
+			contentClick[i].classList.remove("hidden");
+		}
 	}
 });
