fe79fe4bcec9ada907416e87feee8659567a3852
fellipefreiire <fellipefreiire3@gmail.com>
2020-05-07 16:16:49 -0300

    clickContent improve

--- src/js/clickContent.js
+++ src/js/clickContent.js
@@ -1,33 +1,11 @@
 const $content = document.querySelector(".content");
+const $contentTech = document.querySelector(".content-tech");
 
-const $contentHtml = document.querySelector(".content-html");
-const $contentCss = document.querySelector(".content-css");
-const $contentJs = document.querySelector(".content-js");
-const $contentNode = document.querySelector(".content-node");
-const $contentNpm = document.querySelector(".content-npm");
-const $contentSass = document.querySelector(".content-sass");
-const $contentGulp = document.querySelector(".content-gulp");
-const $contentRollup = document.querySelector(".content-rollup");
-const $contentWebpack = document.querySelector(".content-webpack");
-
-const contentClick = [
-	$contentHtml,
-	$contentCss,
-	$contentJs,
-	$contentNode,
-	$contentNpm,
-	$contentSass,
-	$contentGulp,
-	$contentRollup,
-	$contentWebpack,
-];
-
-$content.addEventListener("click", function (e) {
-	$content.className += " hidden";
-
-	for (let i = 0; i < contentClick.length; i++) {
-		if (e.target.parentNode.id === $content.children[i].id) {
-			contentClick[i].classList.remove("hidden");
+$content.addEventListener("click", e => {
+	for (let i = 0; i < $contentTech.children.length; i++) {
+		if ((e.target.parentNode.id || e.srcElement.id) === $content.children[i].id) {
+			$content.className += " hidden";
+			$contentTech.children[i].classList.remove("hidden");
 		}
 	}
 });
